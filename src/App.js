14e1d39732f85380f049d3e305813496afba586f
VedantPC <vedant.naik09@gmail.com>
2023-09-17 21:49:43 +0530

    Commit 17-9-23

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Page2 from './components/Page2';
-
 import Page1 from './components/page1';
-
-
+import Page3 from './components/Page3';
+import Page4 from './components/Page4';
+import Page5 from './components/Page5';
+import Page6 from './components/Page6';
+import Page7 from './components/Page7';
+import Page8 from './components/Page8';
 function App() {
   return (
     <div className="App">
       <Page1></Page1>
       <Page2></Page2>
+      <Page3></Page3>
+      <Page4></Page4>
+      <Page5></Page5>
+      <Page6></Page6>
+      <Page7></Page7>
+      <Page8></Page8>
     </div>
   );
 }
