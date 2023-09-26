ea9c9b6b6257a6f33f29e244aebb22e2d4f275fe
VedantPC <vedant.naik09@gmail.com>
2023-09-26 21:41:45 +0530

    26-9

--- src/components/page1.js
+++ src/components/page1.js
@@ -8,7 +8,7 @@ function page1() {
       <div className='pageTitle'>
       <h1 className='text-white title' style={{}}>HIGH QUALITY <br/> COURSES</h1>
       <h4 className='text-white text-center pg-1-body'>Every day brings with it a fresh set of learning possibilities.</h4>
-      <div style={{display:"flex",justifyContent:"center",}}>
+      <div style={{display:"flex",justifyContent:"space-evenly",width:"30%",margin:"auto"}}>
       <button type="button" className="btn btn-pg-1"><b>Get Quote Now</b></button>
       <button type="button" className="btn btn-pg-1" style={{backgroundColor:"transparent",borderRadius:"5%",border:"1px solid #ff7b52"}}><b>Learn More</b></button>
       </div>
